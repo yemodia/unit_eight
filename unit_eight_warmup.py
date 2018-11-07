45c87a42906d155363c324a1482e2152fc1e85e1
Yerim <yerim@iMac6.local>
2018-11-07 13:28:13 -0500

    Added Labels and Entry Field

--- unit_eight_warmup.py
+++ unit_eight_warmup.py
@@ -3,5 +3,13 @@ import tkinter
 root = tkinter.Tk()
 root.title("Temperature Converter")
 
+f_label = tkinter.Label(root, text="degrees F:")
+f_label.grid(column=1, row=1)
+f_entry = tkinter.Entry(root)
+f_entry.grid(column=2, row=1)
 
+c_label = tkinter.Label(root, text="degrees C:")
+c_label.grid(column=1, row=2)
+c_entry = tkinter.Label(root)
+c_entry.grid(column=2, row=2)
 root.mainloop()
