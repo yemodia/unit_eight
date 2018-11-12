18f3b4f8a7e4b47939a5e7324ac2016b1edd18eb
Yerim <yerim@iMac6.local>
2018-11-12 08:58:47 -0500

    Today I added the random_number function so the program will generate a number for the user to guess. I also added all the labels and buttons necessary such as rhe number_guesses and the guess_statment.

--- assignment_eight.py
+++ assignment_eight.py
@@ -1,9 +1,18 @@
 import tkinter
-
+import random
 
 root = tkinter.Tk()
 root.title("Guess the Number")
+
+def random_number():
+    number = random.randint(1, 100)
+    return number
+
+def user_guess():
+
 guess_value = tkinter.IntVar()
+guess_statement = tkinter.StringVar()
+number_guesses = tkinter.IntVar()
 
 guess_label = tkinter.Label(root, text="Guess a Number from 1-100:")
 guess_label.grid(column=1, row=1)
@@ -13,6 +22,10 @@ guess_entry.grid(column=3, row=1)
 guess_button = tkinter.Button(root, text="Guess")
 guess_button.grid(column=2, row=6)
 
+guess_statement = tkinter.Label(root)
+guess_statement.grid(column=2, row=8)
 
+number_guesses = tkinter.Label(root, text="Number of Guesses:")
+number_guesses.grid(column=1, row=10)
 
 root.mainloop()
