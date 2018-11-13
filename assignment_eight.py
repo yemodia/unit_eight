f1527b8755d475c63290fef227df261a80c649f2
Yerim <yerim@iMac6.local>
2018-11-13 10:26:27 -0500

    Today I added the function to check if the random number is equal to the user guess. I also set some of my abels to zere for when the user first opens up the program.

--- assignment_eight.py
+++ assignment_eight.py
@@ -4,28 +4,45 @@ import random
 root = tkinter.Tk()
 root.title("Guess the Number")
 
-def random_number():
-    number = random.randint(1, 100)
-    return number
+number = random.randint(1, 100)
+total_guesses = 0
 
-def user_guess():
+def answer_check():
+    user_guess = guess_value.get()
+    if user_guess == number:
+        guess_var.set("Congratulations")
+    elif user_guess>number:
+        guess_var.set("The number is too high")
+    elif user_guess<number:
+        guess_var.set("The number is too low")
+
+
+
+
 
 guess_value = tkinter.IntVar()
-guess_statement = tkinter.StringVar()
-number_guesses = tkinter.IntVar()
+guess_var = tkinter.StringVar()
+number_guesses = tkinter.StringVar()
+number_guesses.set(0)
 
 guess_label = tkinter.Label(root, text="Guess a Number from 1-100:")
 guess_label.grid(column=1, row=1)
 guess_entry = tkinter.Entry(root, textvariable=guess_value)
-guess_entry.grid(column=3, row=1)
+guess_entry.grid(column=2, row=1)
 
-guess_button = tkinter.Button(root, text="Guess")
-guess_button.grid(column=2, row=6)
+guess_button = tkinter.Button(root, text="Guess", command=answer_check)
+guess_button.grid(column=1, row=2)
+
+result_label = tkinter.Label(root, textvariable=guess_var)
+result_label.grid(column=2, row=2)
 
 guess_statement = tkinter.Label(root)
-guess_statement.grid(column=2, row=8)
+guess_statement.grid(column=1, row=3, columnspan=2)
 
-number_guesses = tkinter.Label(root, text="Number of Guesses:")
-number_guesses.grid(column=1, row=10)
+number_label = tkinter.Label(root, text="Number of Guesses:")
+number_label.grid(column=1, row=4)
+
+total_label = tkinter.Label(root, textvariable=number_guesses)
+total_label.grid(column=2, row=4)
 
 root.mainloop()
